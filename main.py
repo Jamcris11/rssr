0f30531450189d8e47ff57873892c98905e05882
jamcris11 <jamcris11@hotmail.co.uk>
2025-08-24 08:23:52 +0100

    New: system to pull videos from TikTok (sketchy)

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env ./pvenv/bin/python
 # Copyright (c) 2025 Jim
 # /
 
-from bs4 import BeautifulSoup
+from TikTokApi import TikTokApi
+import asyncio
+import os
 from xml.etree.ElementTree import Element, SubElement, tostring
 import datetime
 import requests
 import json
 
-def download_itemlist_json(url):
-    req = requests.get(url, auth=('user', 'pass'))
+appid = 1988
+count = 35
+ms_token = "iVzn4V-A4bPy3NNJAxLcrBUtEsVOk6qom3pffTEOQ7odCtLvlbqaETWcZ8Ih91FJLHXHIngv_uEcp9nlC3rdT8GwgEfHfnWHeoM9hRcJdqBlRNTCYK4zwr1uHiYeWEFkcznKZk9_3eZRkHYUenM1"
+sec_uid = "MS4wLjABAAAAzXa4i_wZ8O3DdHEsw-sKLizL7VlFfiNvZ9IHI5_U94CB6JNedAdyJWKEd2bmODPM"
+
+payload = {
+    "aid": appid,
+    "count": count,
+    "msToken": ms_token,
+    "cursor": 0,
+    "secUid": sec_uid,
+}
+
+def download_itemlist_json():
+    req = requests.get("https://www.tiktok.com/api/post/item_list/", params=payload)
 
     if req.status_code != 200:
         print(f"Failed with an error of {req.status_code} -> {url}")
@@ -17,10 +32,12 @@ def download_itemlist_json(url):
 
     return json.loads(req.text) 
 
-with open("page.json", "r") as f:
-    _json = json.load(f)
+#with open("page.json", "r") as f:
+#    _json = json.load(f)
 
-root = Element('rss')
+_json = download_itemlist_json()
+
+root = Element("rss", attrib={"version": "2.0"})
 channel = SubElement(root, "channel")
 link = SubElement(channel, "link")
 desc = SubElement(channel, "desc")
